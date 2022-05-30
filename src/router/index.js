cf17f8fa989bca749f081d541804ca7b22e677f2
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-05-30 10:08:16 +0200

    nav v1

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Accueil from '../views/Accueil.vue'
+import Artistes from '../views/Artistes.vue'
+import Billeterie from '../views/Billeterie.vue'
+import Festival from '../views/Festival.vue'
+import Programmation from '../views/Programmation.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    { path: '/', name: 'Accueil', component: Accueil },
+    { path: '/artistes', name: 'Artistes', component: Artistes },
+    { path: '/billeterie', name: 'Billeterie', component: Billeterie },
+    { path: '/festival', name: 'Festival', component: Festival },
+    { path: '/programmation', name: 'Programmation', component: Programmation },
+
   ]
 })
 
---
+++ src/views/Accueil.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex justify-center items-center w-[1442px] relative">
+  <div class="flex-grow h-[1024px]">
+    <div
+      class="flex justify-start items-start w-full absolute left-8 gap-2.5"
+    >
+      <img
+        src="../../public/hero/hero-3.webp"
+        class="flex-grow  object-none"
+      />
+    </div>
+    <div
+      class="flex justify-start items-start absolute overflow-hidden gap-2.5 py-[287px]"
+    >
+      <div class="flex-grow h-[450px] bg-gray-50/90"></div>
+    </div>
+    <div
+      class="flex flex-col justify-start items-start w-[1421.97px] h-[382px] absolute left-[11.52px] top-[320.5px] overflow-hidden gap-[9px]"
+    >
+      <div
+        class="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
+      >
+        <img
+          src="../../public/logo/logo-xl-color.png"
+          class="flex-grow-0 flex-shrink-0 w-80 object-none"
+        />
+      </div>
+      <div
+        class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
+      >
+        <p class="flex-grow-0 flex-shrink-0 text-[80px] text-left">
+          <span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-blue-600">Z</span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#3a3a3a]">énith</span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#8c158e]"> </span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#330458]">R</span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#3a3a3a]">ap </span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#8c158e]">S</span
+          ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#3a3a3a]"
+            >trasbourg</span
+          >
+        </p>
+      </div>
+      <div
+        class="flex justify-end items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
+      >
+        <p class="flex-grow-0 flex-shrink-0 text-5xl text-left capitalize text-[#3a3a3a]">
+          en extérieur
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+    <h1>coucou</h1>
+</template>
+
+<script></script>
