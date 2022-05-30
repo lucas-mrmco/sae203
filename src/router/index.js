0f18f4e134915ba66427b9d7661402dec457a75f
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-05-30 12:42:50 +0200

    link footer

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,10 @@ import Accueil from '../views/Accueil.vue'
 import Artistes from '../views/Artistes.vue'
 import Billeterie from '../views/Billeterie.vue'
 import Festival from '../views/Festival.vue'
-import Programmation from '../views/Programmation.vue'
+import Contact from '../views/Contact.vue'
+import Artiste from '../views/Artiste.vue'
+import Concert from '../views/Concert.vue'
+import Mentions from '../views/Mentions.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +16,10 @@ const router = createRouter({
     { path: '/billeterie', name: 'Billeterie', component: Billeterie },
     { path: '/festival', name: 'Festival', component: Festival },
     { path: '/programmation', name: 'Programmation', component: Programmation },
+    { path: '/contact', name: 'Contact', component: Contact },
+    { path: '/artiste', name: 'Artiste', component: Artiste },
+    { path: '/concert', name: 'Concert', component: Concert },
+    { path: '/mentions', name: 'Mentions', component: Mentions },
 
   ]
 })
