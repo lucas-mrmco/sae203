919e9a634a210a3f2d98004b90a6467179ba13ec
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-05-30 10:59:19 +0200

    hero

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,11 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-<img src="../public/logo/logo-xl-color.png" class="w-7 h-7 max-w-" alt="">
-  <header>
 
-        
-    <!-- <div
-  class="flex justify-start items-start w-[1440px] h-[100px] relative gap-2.5 px-[55px] bg-white"
->
-  <div class="flex-grow h-[100px]">
-    <div
-      class="flex justify-start items-start w-[1330px] absolute left-[54.5px] top-[-0.5px] gap-[30px]"
-    >
-      <img src="image-1.png" class="flex-grow-0 flex-shrink-0 w-[156.5px] h-[100px] object-cover" />
-      <div class="self-stretch flex-grow-0 flex-shrink-0 w-[1198px] relative overflow-hidden">
-        
-</div> -->
-      <nav class="flex fixed justify-center space-x-8">
+  <header class="p-7 text-2xl fixed z-30 bg-white w-full">
+      <nav class="flex justify-center space-x-8 font-league-gothic ">
+        <RouterLink to="/">ACCUEIL </RouterLink>
+        <RouterLink to="/programmation">PROGRAMMATION </RouterLink>
         <RouterLink to="/artistes">ARTISTES </RouterLink>
         <RouterLink to="/festival">LE FESTIVAL </RouterLink>
         <RouterLink to="/artistes">BILLETERIE </RouterLink>
@@ -26,6 +15,8 @@ import { RouterLink, RouterView } from 'vue-router'
   </header>
 
   <RouterView />
+  <footer class="block h-64 bg-fondsite text-white">
+ <Footer1 />
+ </footer>
 </template>
-
 
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="flex justify-center items-center w-[1442px] relative">
-  <div class="flex-grow h-[1024px]">
-    <div
-      class="flex justify-start items-start w-full absolute left-8 gap-2.5"
+    <div class="flex justify-center items-center w-[1442px] ">
+  <div class="flex-grow w-full">
+    <div 
+      class="flex justify-start items-start  absolute left-8 gap-2.5 "
     >
       <img
         src="../../public/hero/hero-3.webp"
-        class="flex-grow  object-none"
+        class="flex-grow  w-full"
       />
     </div>
     <div
       class="flex justify-start items-start absolute overflow-hidden gap-2.5 py-[287px]"
     >
-      <div class="flex-grow h-[450px] bg-gray-50/90"></div>
     </div>
     <div
-      class="flex flex-col justify-start items-start w-[1421.97px] h-[382px] absolute left-[11.52px] top-[320.5px] overflow-hidden gap-[9px]"
+      class="flex flex-col justify-start items-start w-full h-92 absolute left-[11.52px] top-[320.5px] overflow-hidden gap-[9px] bg-gray-50/90"
     >
       <div
-        class="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
+        class="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 "
       >
         <img
           src="../../public/logo/logo-xl-color.png"
-          class="flex-grow-0 flex-shrink-0 w-80 object-none"
+          class="flex-grow-0 flex-shrink-0 w-80 "
         />
       </div>
       <div
-        class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
+        class="flex justify-center  items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
       >
-        <p class="flex-grow-0 flex-shrink-0 text-[80px] text-left">
+        <h1 class="flex-grow-0 flex-shrink-0 text-[80px] text-left font-bangers">
           <span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-blue-600">Z</span
           ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#3a3a3a]">énith</span
           ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#8c158e]"> </span
@@ -38,19 +37,18 @@
           ><span class="flex-grow-0 flex-shrink-0 text-[80px] text-left text-[#3a3a3a]"
             >trasbourg</span
           >
-        </p>
+        </h1>
       </div>
       <div
         class="flex justify-end items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
       >
-        <p class="flex-grow-0 flex-shrink-0 text-5xl text-left capitalize text-[#3a3a3a]">
+        <h2 class="flex-grow-0 flex-shrink-0 text-5xl text-left capitalize text-[#3a3a3a] font-faster-one mr-8">
           en extérieur
-        </p>
+        </h2>
       </div>
     </div>
   </div>
 </div>
-    <h1>coucou</h1>
 </template>
 
 <script></script>
