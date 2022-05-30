0cc22f506816a0b788230b9048c6912bc70810eb
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-05-30 15:35:26 +0200

    home cards v1

---
+++ src/components/Home1.vue
@@ -0,0 +1,92 @@
+<template>
+
+    <div class="">
+ 
+  <div
+    class="flex flex-col justify-start items-start w-[425px] h-[467.97px] absolute left-[40.54px] top-[947.5px] rounded-[10px] bg-gray-50"
+    style="box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1), 0px 4px 6px -2px rgba(0,0,0,0.05);"
+  >
+    <div class="self-stretch flex-grow-0 flex-shrink-0 h-[225px]">
+      <img
+        src="../../public/home/home-3.webp"
+        class="w-[425px] h-[225px] absolute   rounded-tl-[10px] rounded-tr-[10px] object-cover"
+      />
+    </div>
+    <div class="self-stretch flex-grow">
+      <h3
+        class="w-[425px] h-36 text-3xl absolute left-0 top-60 font-league-gothic text-center uppercase text-violet"
+      >
+        3 scènes différentes pour plus de variétés
+      </h3>
+    </div>
+    <div
+      class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 h-40 relative gap-2.5"
+    >
+      <p class="flex-grow w-[425px] font-montserrat text-lg text-center text-violet">
+        De Lacrim à GIMS, en passant par Doria et DJ Abdel, les variétés du rap vous attendent pour
+        des concerts à couper le souffle !
+      </p>
+    </div>
+  </div>
+</div>
+<div class="w-[425px] h-[566.38px]">
+  
+  <div
+    class="flex flex-col justify-start items-start w-[425px] h-[467.97px] absolute left-[508.17px] top-[947.5px] rounded-[10px] bg-gray-50"
+    style="box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1), 0px 4px 6px -2px rgba(0,0,0,0.05);"
+  >
+    <div class="self-stretch flex-grow-0 flex-shrink-0 h-[225px]">
+      <img
+        src="../../public/home/home-2.webp"
+        class="w-[425px] h-[225px] absolute left-[-0.5px] top-[-0.5px] rounded-tl-[10px] rounded-tr-[10px] object-cover"
+      />
+    </div>
+    <div class="self-stretch flex-grow">
+      <h3
+        class="w-[425px] h-36 text-3xl absolute left-0 top-60 font-league-gothic text-center uppercase text-violet"
+      >
+        Des concerts avec une ambiance folle
+      </h3>
+    </div>
+    <div
+      class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 h-40 relative gap-2.5"
+    >
+      <p class=
+        "flex-grow w-[425px] font-montserrat text-lg text-center text-violet h-40" >
+        De Lacrim à GIMS, en passant par Doria et DJ Abdel, les variétés du rap vous attendent pour
+        des concerts à couper le souffle !
+      </p>
+    </div>
+  </div>
+</div>
+<div class="w-[425px] h-[562.76px]">
+  
+  <div
+    class="flex flex-col justify-start items-start w-[425px] h-[467.5px] absolute left-[975.8px] top-[947.5px] rounded-[10px] bg-gray-50"
+    style="box-shadow: 0px 10px 15px -3px rgba(0,0,0,0.1), 0px 4px 6px -2px rgba(0,0,0,0.05);"
+  >
+    <div class="self-stretch flex-grow-0 flex-shrink-0 h-[225px]">
+      <img
+        src="../../public/home/home-1.webp"
+        class="w-[425px] h-[225px] absolute left-[-0.5px] top-[-0.5px] rounded-tl-[10px] rounded-tr-[10px] object-cover"
+      />
+    </div>
+    <div class="self-stretch flex-grow">
+      <h3
+        class="w-[425px] h-36 text-3xl absolute left-0 top-60 font-league-gothic text-center uppercase text-violet"
+      >
+        Un pannel d’artistes  présents
+      </h3>
+    </div>
+    <div
+      class="flex justify-center items-center self-stretch flex-grow-0 flex-shrink-0 h-40 relative gap-2.5"
+    >
+      <p class=
+        "flex-grow w-[425px] font-montserrat text-lg text-center text-violet">
+        De Lacrim à GIMS, en passant par Doria et DJ Abdel, les variétés du rap vous attendent pour
+        des concerts à couper le souffle !
+      </p>
+    </div>
+  </div>
+</div>
+</template>
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -1,20 +1,26 @@
+<script setup>
+import Card from "../components/Card.vue"
+import Home1 from "../components/Home1.vue";
+</script>
+
 <template>
-    <div class="flex justify-center items-center w-[1442px] ">
+<!-- hero -->
+<div class="flex justify-center items-center w-[1442px] ">
   <div class="flex-grow w-full">
     <div 
       class="flex justify-start items-start   left-8 gap-2.5 "
     >
       <img
         src="../../public/hero/hero-3.webp"
-        class="flex-grow  w-full"
+        class="flex-grow  w-full -mb-44 "
       />
     </div>
     <div
-      class="flex justify-start items-start absolute overflow-hidden gap-2.5 py-[287px]"
+      class="flex justify-start items-start absolute overflow-hidden gap-2.5 "
     >
     </div>
     <div
-      class="flex flex-col justify-start items-start w-full h-92 absolute left-[11.52px] top-[320.5px] overflow-hidden gap-[9px] bg-gray-50/90"
+      class="flex flex-col justify-start items-start w-full h-92 absolute  top-[320.5px] overflow-hidden gap-[9px] bg-gray-50/90"
     >
       <div
         class="flex flex-col justify-center items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5 "
@@ -42,13 +48,47 @@
       <div
         class="flex justify-end items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-2.5"
       >
-        <h2 class="flex-grow-0 flex-shrink-0 text-5xl text-left capitalize text-[#3a3a3a] font-faster-one mr-8">
+        <h2  class="flex-grow-0 flex-shrink-0 text-5xl text-left capitalize text-[#3a3a3a] font-faster-one mr-8">
           en extérieur
         </h2>
       </div>
     </div>
   </div>
 </div>
+
+<!-- nav vers pages pricipales avec fond violet en gradient-->
+<img src="../../public/bg/fond.png" class="-mb-96" alt="">
+<Home1 />
+
+<div class="grid-cols-3 flex space-x-40 -mt-96">
+    <!-- premier bouton -->
+     <div class="">
+      <a href="../views/Festival.vue" class="text-xl text-center text-black z-30"> Le festival</a>
+    
+    <div
+      class=" rounded-[10px] border-2 border-black"
+    ></div>
+     </div>
+
+    <!-- 2em bouton -->
+     <div class="">
+    <a href="/artistes" class="text-xl text-center text-black z-30"> Artistes</a>
+    <div
+      class="rounded-[10px] border-2 border-black"
+    ></div>
+  </div>
+
+    <!-- 3em bouton -->
+    <div class="">
+        <a href="/programmation" class=" text-xl text-center text-black z-30 border-2 rounded-lg p-3"> Programmation </a>
+        <div class="rounded-[10px] border-2 border-black"></div>
+    </div>
+</div>
+
+
+<Card/>
+
+
 </template>
 
 <script></script>
